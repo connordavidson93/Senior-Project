d6d8555c432b977f3a33037e44cefdca24d53f9a
Alexa <mouldy.k@gmail.com>
2019-10-26 14:52:34 -0600

    added AlexaMain scene, started on arena system (almost done!), added .dwlt to .gitignore

---
+++ SeniorProjectUnity/Assets/Scripts/Arena/ArenaEnemyTracker.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ArenaEnemyTracker : MonoBehaviour
+{
+    public List<GameObject> enemies;
+    public UnityEvent enemiesClearedEvent;
+    private bool canCheck;
+
+    public void StartCheck()
+    {
+        canCheck = true;
+        StartCoroutine(Check());
+    }
+
+    public void StopCheck()
+    {
+        canCheck = false;
+    }
+
+    IEnumerator Check()
+    {
+        while(canCheck)
+        {
+            bool allDead = true;
+            foreach(GameObject enemy in enemies)
+            {
+                if(enemy.activeSelf)
+                {
+                    allDead = false;
+                    break;
+                }
+            }
+            if(allDead)
+            {
+                enemiesClearedEvent.Invoke();
+            }
+            yield return StaticVars.oneHundredth;
+        }
+    }
+}
---
+++ SeniorProjectUnity/Assets/Scripts/Generics/OnTriggerEnterEvent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnTriggerEnterEvent : MonoBehaviour
+{
+    public UnityEvent onTriggerEvent;
+    public bool useTag;
+    public string tag;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (useTag)
+        {
+            if(other.tag == tag)
+            {
+                onTriggerEvent.Invoke();
+            }
+        }
+        else
+        {
+            onTriggerEvent.Invoke();
+        }
+    }
+}
