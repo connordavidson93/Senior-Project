a081f51df1d3644e9c4bdff91c2ef04573225308
Alexa <mouldy.k@gmail.com>
2019-10-29 14:54:53 -0600

    added highlight shader for enemies and ram order

---
+++ SeniorProjectUnity/Assets/Scripts/VFX/HighlightObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class HighlightObject : MonoBehaviour
+{
+	public Material base_mat;
+	public Material highlight_mat;
+    public Renderer rend;
+
+	void OnMouseOver()
+	{
+		rend.material = highlight_mat;
+	}
+
+	void OnMouseExit()
+	{
+		rend.material = base_mat;
+	}
+}
